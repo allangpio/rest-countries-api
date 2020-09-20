ff88e56f533710e566190dab92168320326870ae
allangpio <gpioallan@gmail.com>
2020-09-20 17:42:34 -0300

    creating global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  body {
+    background: #312e38;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input {
+    font: 16px 'Nunito', 'sans-serif';
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+`;
