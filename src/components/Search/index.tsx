6a4d669be15154e44a5858ad1ccdc345a2f5c24b
allangpio <gpioallan@gmail.com>
2020-09-21 21:06:01 -0300

    creating coutri cards component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 
-import { SearchContainer, TextInputContainer } from './styles';
+import { Container, TextInputContainer } from './styles';
 
 const Search: React.FC = () => {
   return (
-    <SearchContainer>
+    <Container>
       <TextInputContainer>
         <FaSearch />
         <input type="text" placeholder='Search for a country...' />
       </TextInputContainer>
 
 
-      <select name="region" id="region">
-        <option value="0" disabled selected hidden>Find by Region...</option>
+      <select name="region" id="region" defaultValue={0} >
+        <option value="0" disabled hidden>Find by Region...</option>
         <option value="africa">Africa</option>
         <option value="america">America</option>
         <option value="asia">Asia</option>
@@ -21,7 +21,7 @@ const Search: React.FC = () => {
         <option value="ocenia">Ocenia</option>
       </select>
 
-    </SearchContainer>
+    </Container>
   );
 };
 
--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const SearchContainer = styled.div`
+export const Container = styled.div`
  display: flex;
  justify-content: space-between;
  margin-top: 1rem;
