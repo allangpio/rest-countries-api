e1515f3d1dc548a4adec52699c1f493027181304
allangpio <gpioallan@gmail.com>
2020-09-21 21:07:08 -0300

    creating Card component folder

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+import { Container, Info } from './styles';
+
+interface CountrieProps {
+
+  name: string;
+  flag: string;
+  region: string;
+  population: number;
+  capital: string;
+
+
+}
+
+const Card: React.FC<CountrieProps> = ({ name, flag, region, population, capital }: CountrieProps) => {
+  return (
+    <Container>
+      <img src={flag} alt={`{name} flag`} />
+      <Info>
+        <strong>{name}</strong> <br />
+        <p><span>Population: </span> {population}</p>
+        <p><span>Region: </span> {region}</p>
+        <p><span>Capital: </span>{capital}</p>
+      </Info>
+
+
+    </Container>);
+}
+
+export default Card;
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+ width: 250px;
+
+ border-radius: 8px;
+ overflow: hidden;
+ box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+
+ background: ${props => props.theme.colors.elements};
+
+ margin-top: 1rem;
+ img {
+   width: 100%;
+   height: 166px;
+
+ }
+`;
+
+export const Info = styled.div`
+padding: 0.5rem 1rem;
+  strong {
+    font-size: 16px;
+    display: block;
+  }
+
+  span {
+    font-weight: 600
+  }
+
+  span, p {
+    font-size: 12px;
+  }
+
+  p {
+    display: block;
+    margin-bottom: .2rem;
+  }
+`;
