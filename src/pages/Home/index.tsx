3d3d6f9112f27d23beb6dc66f51f4f030377f97e
allangpio <gpioallan@gmail.com>
2020-09-21 21:06:44 -0300

    importing cards and rendering coutries from the api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api'
 
 import Search from '../../components/Search';
-import { Container } from './styles';
+import Card from '../../components/Card';
+
+import { Container, CardsContainer } from './styles';
+
+interface CountrieProps {
+  name: string;
+  flag: string;
+  region: string;
+  population: number;
+  capital: string;
+  numericCode: string;
+}
 
 const Home: React.FC = () => {
+  const [countries, setCountries] = useState<CountrieProps[]>({} as CountrieProps[]);
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    (async function getCountries() {
+      const response = await api.get('/all');
+      setCountries(response.data);
+      setLoading(true);
+    })();
+  }, [])
+
   return (
     <Container>
       <Search />
+      <CardsContainer>
+        {!loading ? <h1>Loading</h1> : (countries.map(countrie => (
+          <Card key={countrie.numericCode} name={countrie.name} flag={countrie.flag} region={countrie.region} population={countrie.population} capital={countrie.capital} />
+        )))}
+      </CardsContainer>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,3 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding: 1rem 4rem;
 `;
+
+export const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 250px);
+  gap: 4rem;
+  margin-top: 2rem;
+
+  place-content: center;
+`;
