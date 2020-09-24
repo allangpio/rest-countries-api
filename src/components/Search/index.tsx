8114fdaad0a064b55b8ecd4d38426db579b82328
allangpio <gpioallan@gmail.com>
2020-09-23 23:49:41 -0300

    using countries context to filter data based on input change

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useCountry } from '../../hooks/countries';
 import { FaSearch } from 'react-icons/fa';
 
 import { Container, TextInputContainer } from './styles';
 
 const Search: React.FC = () => {
+
+  const { filterCountries, filterByRegion } = useCountry();
+  const handleInput = useCallback((e) => {
+    filterCountries(e.target.value)
+  }, [filterCountries])
+
+  const handleSelect = useCallback((e) => {
+    console.log(e.target.value)
+    filterByRegion(e.target.value)
+  }, [filterByRegion])
+
   return (
     <Container>
       <TextInputContainer>
         <FaSearch />
-        <input type="text" placeholder='Search for a country...' />
+        <input type="text" onChange={handleInput} placeholder='Search for a country...' />
       </TextInputContainer>
 
 
-      <select name="region" id="region" defaultValue={0} >
-        <option value="0" disabled hidden>Find by Region...</option>
+      <select name="region" id="region" defaultValue={"none"} onChange={handleSelect} >
+        <option value="none">Find by Region...</option>
         <option value="africa">Africa</option>
-        <option value="america">America</option>
+        <option value="americas">America</option>
         <option value="asia">Asia</option>
         <option value="europe">Europe</option>
-        <option value="ocenia">Ocenia</option>
+        <option value="oceania">Ocenia</option>
       </select>
 
     </Container>
