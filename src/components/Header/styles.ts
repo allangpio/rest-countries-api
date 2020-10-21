26220ecadc9a6e185a90bd44a3e01cf7beb84d53
allangpio <gpioallan@gmail.com>
2020-10-21 15:52:44 -0300

    Making header responsive

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,14 +5,24 @@ export const Container = styled.header`
   height: 4rem;
   padding: 1rem 4rem;
 
+  @media screen and (max-width: 600px)  {
+    padding: 1rem 2rem;
+  }
+
   background: ${props => props.theme.colors.elements};
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 
   display: flex;
   justify-content: space-between;
+  align-items: center;
 
   h1 {
     font-size: 1.5rem;
+
+    @media screen and (max-width: 600px)  {
+      font-size: 1rem;
+    }
+
   }
 
   div {
